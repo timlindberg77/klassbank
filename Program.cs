2bf12d93cdbb08265d6333a4d7d71a7689886607
timlindberg77 <tim.lindberg888@gmail.com>
2026-09-14 12:32:01 +0200

    skapa en klass folder

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Övning1.klass;
 
 namespace Övning1
 {
@@ -26,35 +27,6 @@ namespace Övning1
             v1.vipnummer = "V12345";
 
             v1.run();
-        }
-    }
-    public class kunder
-    {
-        //detta är en klass som innehåller information om kunder
-       public string namn;
-        public string adress;
-        public string telefonnummer;
-        public string email;
-        public string personnummer;
-        public string kundnummer;
-        
-        public void run()
-        {
-            Console.WriteLine("Hej vänligen fyll i kundinformation");
-        }
-    }
-    public class vip : kunder // skapar vip kunder som ärver från kunder klassen
-    {
-        public string vipnummer; //ny attribut för klassen
-
-        public new void run() //ny metod för vip kunder
-        {
-            Console.WriteLine("Hej vänligen fyll i VIP kundinformation");
-        }   
-
-        public void treatment() //ny metod för vip kunder
-        {
-            Console.WriteLine("VIP kunder får bättre behandling");
         }
     }
 }
---
+++ klass/klasser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Övning1.klass
+{
+    public class kunder
+    {
+        //detta är en klass som innehåller information om kunder
+        public string namn;
+        public string adress;
+        public string telefonnummer;
+        public string email;
+        public string personnummer;
+        public string kundnummer;
+
+        public void run()
+        {
+            Console.WriteLine("Hej vänligen fyll i kundinformation");
+        }
+    }
+    public class vip : kunder // skapar vip kunder som ärver från kunder klassen
+    {
+        public string vipnummer; //ny attribut för klassen
+
+        public new void run() //ny metod för vip kunder
+        {
+            Console.WriteLine("Hej vänligen fyll i VIP kundinformation");
+        }
+
+        public void treatment() //ny metod för vip kunder
+        {
+            Console.WriteLine("VIP kunder får bättre behandling");
+        }
+    }    
+}
