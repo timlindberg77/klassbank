9d7948473e085b78ebb633c5be101c3ae6a49491
timlindberg77 <tim.lindberg888@gmail.com>
2026-09-14 11:26:04 +0200

    skapat kund

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Övning1
+﻿using System;
+
+namespace Övning1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            kunder k1 = new kunder();
+
+            k1.namn = "Janne";
+            k1.adress = "Storgatan 1";
+            k1.telefonnummer = "0701234567";
+            k1.personnummer = "123456-7890";
+            k1.kundnummer = "K12345";
+
+
+        }
+    }
+    public class kunder
+    {
+       public string namn;
+        public string adress;
+        public string telefonnummer;
+        public string email;
+        public string personnummer;
+        public string kundnummer;
+        
+        public void run()
+        {
+            Console.WriteLine("Hej vänligen fyll i kundinformation");
         }
     }
 }
